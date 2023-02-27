1d7897249e6c4b70c042280d50351abe5a6b70bd
AlWestwood1 <zceeawe@ucl.ac.uk>
2023-02-27 18:11:32 +0000

    Added multi-threading

--- client.py
+++ client.py
@@ -2,12 +2,30 @@
 
 import socket
 
-HOST = '127.0.0.1'
-PORT = 65432
+def Main():
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
 
-print('Received', repr(data))
+    HOST = '127.0.0.1'
+    PORT = 65432
+
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+
+            message = "Hello, world"
+            s.send(message.encode("ascii"))
+            
+            data = s.recv(1024)
+
+        print('Received:', str(data.decode("ascii")))
+
+        ans = input("\nDo you want to continue? (y/n): ")
+        if ans == 'y':
+            continue
+        else:
+            break
+
+    s.close()
+
+if __name__ == '__main__':
+    Main()
