0bdf07cac8009501149b1ca80a0f6d02a99dff4e
StAandrew <dev.staniukynas@gmail.com>
2023-03-01 15:46:30 +0000

    fixed syntax

--- client.py
+++ client.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python3
-
 import socket
 
-def Main():
+
+HOST = "127.0.0.1"
+PORT = 65432
 
 
-    HOST = '127.0.0.1'
-    PORT = 65432
-
-    while True:
+def main():
+    continue_loop = True
+    while continue_loop:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((HOST, PORT))
 
             message = "Hello, world"
             s.send(message.encode("ascii"))
-            
+
             data = s.recv(1024)
-
-        print('Received:', str(data.decode("ascii")))
-
-        ans = input("\nDo you want to continue? (y/n): ")
-        if ans == 'y':
-            continue
-        else:
-            break
-
+        print("Received:", str(data.decode("ascii")))
+        answer_valid = False
+        while not answer_valid:
+            ans = input("Do you want to continue? (y/n): ")
+            if ans == "y":
+                continue_loop = True
+                answer_valid = True
+            elif ans == "n":
+                continue_loop = False
+            else:
+                print("Invalid answer. Enter 'y' or 'n'.")
     s.close()
 
-if __name__ == '__main__':
-    Main()
+
+if __name__ == "__main__":
+    main()
