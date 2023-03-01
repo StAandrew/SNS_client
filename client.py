c006c9934bc466581767955827a190491754f5b7
StAandrew <dev.staniukynas@gmail.com>
2023-03-01 16:05:35 +0000

    split into functions

--- client.py
+++ client.py
@@ -6,29 +6,40 @@ HOST = "127.0.0.1"
 PORT = 65432
 
 
+def send_message(message, encoding="ascii"):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.connect((HOST, PORT))
+            s.send(message.encode(encoding))
+            data = s.recv(1024)
+            print("Received:", str(data.decode(encoding)))
+            s.close()
+        except ConnectionRefusedError:
+            print("Connection refused. Server not running?")
+
+
+def continue_loop_from_keyboard():
+    answer_valid = False
+    while not answer_valid:
+        try:
+            ans = input("Do you want to continue? (y/n): ")
+        except KeyboardInterrupt:
+            print("\nBye")
+            return False
+        if ans == "y":
+            return True
+        elif ans == "n":
+            return False
+        else:
+            print("Invalid answer. Enter 'y' or 'n'.")
+
+
 def main():
     continue_loop = True
     while continue_loop:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((HOST, PORT))
-
-            message = "Hello, world"
-            s.send(message.encode("ascii"))
-
-            data = s.recv(1024)
-        print("Received:", str(data.decode("ascii")))
-        answer_valid = False
-        while not answer_valid:
-            ans = input("Do you want to continue? (y/n): ")
-            if ans == "y":
-                continue_loop = True
-                answer_valid = True
-            elif ans == "n":
-                continue_loop = False
-            else:
-                print("Invalid answer. Enter 'y' or 'n'.")
-    s.close()
-
+        message = "Hello, world"
+        send_message(message)
+        continue_loop = continue_loop_from_keyboard()
 
 if __name__ == "__main__":
     main()
