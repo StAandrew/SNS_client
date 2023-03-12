df595db835921041416945197987587e6c7291f1
AlWestwood1 <zceeawe@ucl.ac.uk>
2023-03-12 02:05:44 +0000

    added menu selection

--- client.py
+++ client.py
@@ -3,16 +3,28 @@ import socket
 
 
 HOST = "127.0.0.1"
-PORT = 65432
+PORT = 65433
 
 
-def send_message(message, encoding="ascii"):
+def send_stock(message, stock):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         try:
             s.connect((HOST, PORT))
-            s.send(message.encode(encoding))
+            s.send(f"{message},{stock}".encode()) #Message sent is a tuple
             data = s.recv(1024)
-            print("Received:", str(data.decode(encoding)))
+            print("Received:", str(data.decode()))
+            s.close()
+        except ConnectionRefusedError:
+            print("Connection refused. Server not running?")
+
+
+def send_message(message,):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.connect((HOST, PORT))
+            s.send(f"{message},{0}".encode()) #0 sent instead of real stock as it is not needed
+            data = s.recv(1024)
+            print("Received:", str(data.decode()))
             s.close()
         except ConnectionRefusedError:
             print("Connection refused. Server not running?")
@@ -37,8 +49,18 @@ def continue_loop_from_keyboard():
 def main():
     continue_loop = True
     while continue_loop:
-        message = "Hello, world"
-        send_message(message)
+        try:
+            choice = int(input("Please select one of the following options:\n 1. Fetch Stock Data\n 2. Predict stock price\n 3. Daily summary\n"))
+        except TypeError:
+            print("Invalid input, please try again.\n")
+        if choice == 1 or choice == 2:
+            stock = input("Which stock would you like to select? ")
+            send_stock(choice, stock)
+        elif choice == 3:
+            send_message(choice)
+        else:
+            print("Invalid input, please try again.\n")
+
         continue_loop = continue_loop_from_keyboard()
 
 if __name__ == "__main__":
