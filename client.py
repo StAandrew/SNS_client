f400b31c0819824fa315727b6632de5fd8bd8473
AlWestwood1 <zceeawe@ucl.ac.uk>
2023-04-01 22:48:05 +0100

    Basic integration with ML algorithm

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ import json
 import string
 import numpy as np
 import pandas as pd
+import pickle
 import tensorflow as tf
 from sklearn.preprocessing import LabelEncoder
 from keras_preprocessing.text import Tokenizer
@@ -12,32 +13,29 @@ from keras_preprocessing.sequence import pad_sequences
 from keras.layers import Embedding, LSTM, Dense, Flatten, SpatialDropout1D
 from keras.models import Sequential
 import matplotlib.pyplot as plt
-
+import os
+from config import figures_dir, dataset_dir
 
 HOST = "127.0.0.1"
-PORT = 65433
+PORT = 65432
+
+def get_dir(ticker):
+    stock_csv = ticker + "_data.csv"
+    stock_dir = os.path.join(dataset_dir, stock_csv)
+    return stock_dir
 
 
-def send_stock(message, stock):
+def send_stock(stock):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         try:
             s.connect((HOST, PORT))
-            s.send(f"{message},{stock}".encode()) #Message sent is a tuple
-            data = s.recv(1024)
-            print("Received:", str(data.decode()))
+            stock_pickle = pickle.dumps(stock)
+            s.send(stock_pickle) #Message sent as a pickle object
+            data_pickle = s.recv(1024)
+            data = pickle.loads(data_pickle)
+            #print("Received:", str(data.decode()))
             s.close()
-        except ConnectionRefusedError:
-            print("Connection refused. Server not running?")
-
-
-def send_message(message,):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        try:
-            s.connect((HOST, PORT))
-            s.send(f"{message},{0}".encode()) #0 sent instead of real stock as it is not needed
-            data = s.recv(1024)
-            print("Received:", str(data.decode()))
-            s.close()
+            return data
         except ConnectionRefusedError:
             print("Connection refused. Server not running?")
 
@@ -57,7 +55,17 @@ def continue_loop_from_keyboard():
         else:
             print("Invalid answer. Enter 'y' or 'n'.")
 
-
+def plot_prediction(ticker, predictions):
+    stock_dir = get_dir(ticker)
+    dataset = pd.read_csv(stock_dir, index_col=0)
+    plt.plot(dataset.tail(60).index, dataset.tail(60)['Close'], color = 'red', label = f'Historical {ticker} Price')
+    plt.plot(predictions.index, predictions['Close'], color = 'blue', label = f'Predicted {ticker} Price')
+    plt.title(f'{ticker} Stock Price Prediction')
+    plt.xlabel('Time')
+    plt.ylabel(f'{ticker} Stock Price')
+    plt.legend()
+    plt.savefig(os.path.join(figures_dir, ticker + "_prediction.png"))
+    plt.show()
 
 def main():
     #Load data from .json file
@@ -132,17 +140,112 @@ def main():
         tag = encoder.inverse_transform([output])[0]
         print(random.choice(responses[tag]))
 
-        if tag == "new_stock":
+        stockList = []
+        if tag == "get_price":
             stock = input("Which stock would you like to select? ")
-            send_stock(1, stock)
-        elif tag == "stock_info":
+            days = input("How many days ahead should I predict? ")
+            stockList.append(stock)
+            stockList.append(days)
+            stockList.append('1')
+            data_rec = send_stock(stockList)
+            print(f"The closing price of {stock} is ${data_rec}\n")
+
+        elif tag == "get_daily_return":
+            valid = False
+            num = 1
+            while valid == False:
+                stock = input(f"Please enter stock {num} (or type 'Done' to finish): ")
+                if stock.lower() == "done":
+                    valid = True
+                else:
+                    stockList.append(stock)
+                    num += 1
+            days = input("How many days ahead should I predict? ")
+            stockList.append(days)
+            data_rec = []
+            if len(stockList) == 2: #Checks that the list only contains one stock (and the number of days to predict)
+                stockList.append('2')
+                data_rec = send_stock(stockList)
+            else:
+                stockList.append('6')
+                data_rec = send_stock(stockList)
+            
+            print(f"The daily returns for your chosen stocks are:\n{data_rec} ")
+
+        elif tag == "get_avg_return":
             stock = input("Which stock would you like to select? ")
-            send_stock(2, stock)
-        elif tag == "predict_stock":
+            days = input("How many days ahead should I predict? ")
+            stockList.append(stock)
+            stockList.append(days)
+            stockList.append('3')
+            data_rec = send_stock(stockList)
+            print(f"The average return of {stock} over {days} days is ${data_rec}\n")
+
+        elif tag == "get_std":
             stock = input("Which stock would you like to select? ")
-            send_stock(3, stock)
-        elif tag == "rundown":
-            send_message(4)
+            days = input("How many days ahead should I predict? ")
+            stockList.append(stock)
+            stockList.append(days)
+            stockList.append('4')
+            data_rec = send_stock(stockList)
+            print(f"The volatility of {stock} over {days} days is {data_rec}\n")
+
+        elif tag == "get_sharpe":
+            stock = input("Which stock would you like to select? ")
+            days = input("How many days ahead should I predict? ")
+            rfr = input("What risk free rate would you like to use? (between 0 and 1): ")
+            stockList.append(stock)
+            stockList.append(rfr) #appended before days and choice so that the server can process the data correctly
+            stockList.append(days)
+            stockList.append('5')
+            data_rec = send_stock(stockList)
+            print(f"The sharpe ratio of {stock} over {days} days is {data_rec}\n")
+
+        elif tag == "optimise":
+            valid = False
+            num = 1
+            while valid == False:
+                stock = input(f"Please enter stock {num} (or type 'Done' to finish): ")
+                if stock.lower() == "done":
+                    valid = True
+                else:
+                    stockList.append(stock)
+                    num += 1
+            days = input("How many days ahead should I predict? ")
+            
+            valid = False
+            while valid == False:
+                try:
+                    optimise_type = int(input("Would you like to optimise for 1. minimum variance or 2. maximum sharpe ratio? (please type 1 or 2 to select option): "))
+                except TypeError:
+                    print("Invalid input, please try again.\n")
+                if optimise_type == 1 or optimise_type == 2:
+                    valid = True
+                else:
+                    print(f"{optimise_type} is not a valid input, please enter 1 or 2 accordingly.\n")
+            if optimise_type == 1:
+                stockList.append(days)
+                stockList.append('7')
+                data_rec = send_stock(stockList)
+                min_var = data_rec.pop()
+
+                print(f"Here are the optimised weights: {data_rec}")
+                print(f"This produces a variance of {min_var}")
+            else:
+                rfr = input("What risk free rate would you like to use? (between 0 and 1): ")
+                stockList.append(rfr)
+                stockList.append(days)
+                stockList.append('8')
+                data_rec = send_stock(stockList)
+                max_sharpe = data_rec.pop()
+
+                print(f"Here are the optimised weights: {data_rec}")
+                print(f"This produces a sharpe ratio of {max_sharpe}")
+
+            
+
+
+
         elif tag == "goodbye":
             quit()
 
