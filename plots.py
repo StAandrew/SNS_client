008ecfd5855446f762ced67627bcaf591f98fac1
Rafael Miguel Asensio Delgado <rasensio.d@gmail.com>
2023-04-06 18:18:57 +0100

    plotting fixed

--- plots.py
+++ plots.py
@@ -16,18 +16,21 @@ def plot_prediction(ticker, predictions):
 
     fig, ax = plt.subplots()
     
-    ax.plot(dataset.tail(60).index, dataset.tail(60)['Close'], color='red', label=f'Historical {ticker} Price')
-    ax.plot(predictions.index, predictions['Close'], color='blue', label=f'Predicted {ticker} Price')
+    ax.plot(dataset.tail(60).index, dataset.tail(60)['Close'], color='red', label=f'Historical Price')
+    ax.plot(predictions.index, predictions['Close'], color='blue', label=f'Predicted Price')
     ax.set_title(f'{ticker} Price Prediction')
     ax.set_xlabel('Time')
     ax.set_ylabel(f'{ticker} Price')
+    plt.legend(loc='best')
+    plt.grid()
     
     # Set the x-axis to display one label per month
     ax.xaxis.set_major_locator(mdates.MonthLocator())
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
     
     # Rotate the x-axis labels for better readability
-    plt.setp(ax.xaxis.get_majorticklabels(), rotation=45, ha='right')
+    plt.setp(ax.xaxis.get_majorticklabels(), ha='right')
+    plt.savefig("Plots/test_prediction.png")
     
     ax.legend()
 
@@ -39,39 +42,46 @@ def plot_daily_returns(ticker, predictions):
     fig, ax = plt.subplots()
     
     ax.plot(predictions.index, predictions['Close'], color='blue')
+    plt.axhline(y=0, color='k', linestyle='-')
     ax.set_title(f'{ticker} Daily Return Prediction')
     ax.set_xlabel('Time')
     ax.set_ylabel(f'{ticker} Daily Return')
+    plt.grid()
     
     # Set the x-axis to display one label per month
     ax.xaxis.set_major_locator(mdates.MonthLocator())
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
     
     # Rotate the x-axis labels for better readability
-    plt.setp(ax.xaxis.get_majorticklabels(), rotation=45, ha='right')
+    plt.setp(ax.xaxis.get_majorticklabels(), ha='right')
+    plt.savefig("Plots/test_daily_ret.png")
     
     ax.legend()
 
 def plot_daily_portfolio_returns(ticker_list, combined_returns):    # feed combined returns df
 
     combined_returns.index = pd.to_datetime(combined_returns.index)
-    total_returns = combined_returns.sum(axis=1)
+    total_returns = combined_returns.sum(axis=1).reset_index(name='Total')
 
     fig, ax = plt.subplots()
 
     for ticker in ticker_list:
         ax.plot(combined_returns.index, combined_returns[ticker], label=f'{ticker}')
-    ax.plot(total_returns.index, total_returns.iloc[:,0], linewidth=2, label=f'Portfolio')
+    ax.plot(combined_returns.index, total_returns['Total'], linewidth=2, label=f'Portfolio')
+    plt.axhline(y=0, color='k', linestyle='-')
     ax.set_title(f'Portfolio Daily Return Prediction')
     ax.set_xlabel('Time')
     ax.set_ylabel(f'Daily Returns')
-    
+    plt.legend(loc='best')
+    plt.grid()
+
     # Set the x-axis to display one label per month
     ax.xaxis.set_major_locator(mdates.MonthLocator())
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
     
     # Rotate the x-axis labels for better readability
-    plt.setp(ax.xaxis.get_majorticklabels(), rotation=45, ha='right')
+    plt.setp(ax.xaxis.get_majorticklabels(), ha='right')
+    plt.savefig("Plots/test_daily_port_ret.png")
     
     ax.legend()
 
@@ -79,23 +89,27 @@ def plot_cumulative_portfolio_returns(ticker_list, combined_returns):
 
     combined_returns.index = pd.to_datetime(combined_returns.index)
     cumulative_returns = combined_returns.cumsum()
-    total_returns = cumulative_returns.sum(axis=1)
+    total_returns = cumulative_returns.sum(axis=1).reset_index(name='Total')
 
     fig, ax = plt.subplots()
 
     for ticker in ticker_list:
         ax.plot(cumulative_returns.index, cumulative_returns[ticker], label=f'{ticker}')
-    ax.plot(total_returns.index, total_returns.iloc[:,0], linewidth=2, label=f'Portfolio')
+    ax.plot(cumulative_returns.index, total_returns['Total'], linewidth=2, label=f'Portfolio')
+    plt.axhline(y=0, color='k', linestyle='-')
     ax.set_title(f'Portfolio Cumulative Return Prediction')
     ax.set_xlabel('Time')
     ax.set_ylabel(f'Returns')
+    plt.legend(loc='best')
+    plt.grid()
     
     # Set the x-axis to display one label per month
     ax.xaxis.set_major_locator(mdates.MonthLocator())
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
     
     # Rotate the x-axis labels for better readability
-    plt.setp(ax.xaxis.get_majorticklabels(), rotation=45, ha='right')
+    plt.setp(ax.xaxis.get_majorticklabels(), ha='right')
+    plt.savefig("Plots/test_cum_port_ret.png")
     
     ax.legend()
 
@@ -105,5 +119,7 @@ def plot_opt_portfolio(ticker_list, weights, type):
     ax.pie(weights, labels=ticker_list, autopct='%1.1f%%')
     if type == 'var':
         ax.set_title(f'Minimum Variance Portfolio')
+        plt.savefig("Plots/min_var_test.png")
     elif type == 'sharpe':
         ax.set_title(f'Maximum Sharpe Portfolio')
+        plt.savefig("Plots/test_max_sharpe.png")
